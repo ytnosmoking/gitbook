bc74c709ace4822891ef9913b9764e39f21e3bf9
yt <349915792@qq.com>
2022-06-06 14:47:51 +0800

    clearn docs

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+gitbook build ./ ./books
+
+# 进入生成的文件夹
+cd ./books
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/ytnosmoking/gitbook.git master:gh-pages
